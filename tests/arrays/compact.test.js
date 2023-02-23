1477da93802361310e918c1bfbd6707eb719571d
Callum Shaw <callum0shaw@gmail.com>
2023-02-23 19:38:31 +0200

    feat: implement copact

--- tests/arrays/compact.test.js
+++ tests/arrays/compact.test.js
@@ -4,6 +4,6 @@ describe('compact', () => {
   const arr = [0, 1, false, 3, 4, '', NaN, undefined, null];
   test('should remove falsy values', () => {
     const res = compact(arr);
-    expect(res).toStrictEqual([0, 1, 3, 4]);
+    expect(res).toStrictEqual([1, 3, 4]);
   });
 });
