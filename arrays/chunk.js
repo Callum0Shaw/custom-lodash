6bdb02351fcb09613439b9e124562bda4c0867ff
Callum Shaw <callum0shaw@gmail.com>
2023-02-23 19:08:48 +0200

    feat: implement chunk

--- arrays/chunk.js
+++ arrays/chunk.js
@@ -8,10 +8,13 @@
  *
  * @return {Array} An array of chunks
  */
+
 function chunk(arr, size = 1) {
   if (size < 1) return [];
+
   let newArr = [];
   let tempArr = [];
+
   for (let i = 1; i <= arr.length; i += 1) {
     tempArr = [...tempArr, arr[i - 1]];
     if (i % size === 0 || i === arr.length) {
@@ -19,7 +22,6 @@ function chunk(arr, size = 1) {
       tempArr = [];
     }
   }
-  console.log(newArr);
   return newArr;
 }
 
--- arrays/compact.js
+++ arrays/compact.js
@@ -1,5 +1,16 @@
-function compact(params) {
-  // TODO:
-  console.log('compact');
+/**
+ * Creates an array with all falsey values removed.
+ * The values false, null, 0, "", undefined, and NaN are falsey.
+ *
+ * @param  {Array} arr array to be processed
+ *
+ * @return {Array} array missing all falsy values
+ */
+function compact(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i]) newArr = [...newArr, arr[i]];
+  }
+  return newArr;
 }
 module.exports = compact;
